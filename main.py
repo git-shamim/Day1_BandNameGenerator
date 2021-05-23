2bb09640344fb8712fb64352d4912fd757f5e8c0
shamim-simplilearn <shamim.gcloud@gmail.com>
2021-05-23 16:59:47 +0530

    day1

---
+++ main.py
@@ -0,0 +1,5 @@
+
+print("Welcome to the band name Generator")
+city = input("Please enter the name of your city:\n")
+pet_name = input("What is the name of your pet?\n")
+print("The name of your band name could be" + " " + city + " " + pet_name)
